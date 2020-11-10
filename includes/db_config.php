cb9afec00f51a365a94695d226d79b0da65aad06
Uthman <dappergeek0@gmail.com>
2020-11-11 00:52:15 +0100

    Implemented database connection in file db_config.php. Included file in header.php and initialized session

--- includes/db_config.php
+++ includes/db_config.php
@@ -1,1 +1,7 @@
 <?php
+    define('host', 'localhost');
+    define('username', 'root');
+    define('password', '');
+    define('database', 'dbcart');
+
+    $connection = new mysqli(host, username, password, database) or die('Cannot connect to server');
--- views/header.php
+++ views/header.php
@@ -1,3 +1,7 @@
+<?php
+    session_start();
+    include 'includes/db_config.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
