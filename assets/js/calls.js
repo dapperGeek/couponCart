0e7517b39b4e2bbd51e53137763d652d5abc82e3
Uthman <dappergeek0@gmail.com>
2020-11-11 00:54:03 +0100

    Imported assets folder for css, js and bootsrap integration

---
+++ assets/js/calls.js
@@ -0,0 +1,21 @@
+let cartTotal = 0;
+
+function addToCart(val) {
+    let id = val[0];
+    let price = val[1];
+    cartTotal += price ;
+    console.log('The price is : ' + val[1] + ' : New Total is ' + cartTotal + ' : ID is ' + id);
+
+    $.ajax({
+        type: 'POST',
+        url:'includes/calls.php',
+        data: {
+            'addToCart' : 1,
+            'id' : id
+        },
+        success: function (data) {
+            // let selected = response['id'];
+            console.log(data);
+        }
+    })
+}
